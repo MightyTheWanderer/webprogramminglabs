5d02db18a7bbc97c57a4f7c7514a88597694bdc0
MightyTheWanderer <39923450+MightyTheWanderer@users.noreply.github.com>
2020-09-14 23:16:33 -0400

    Update oop.js

--- oop.js
+++ oop.js
@@ -1,12 +1,10 @@
-function Movie (name, genre, year)
-{
-  this.name= name;
-  this.genre=genre;
-  this.year=year;
+function Movie(name, genre, year) {
+    this.name = name;
+    this.genre = genre;
+    this.year = year;
 }
-Movie.protoype.print=function()
-{
-  console.log(`${this.name} is a ${this.genre} that was released in ${this.year}`);
+Movie.protoype.print = function () {
+    console.log(`${this.name} is a ${this.genre} that was released in ${this.year}`);
 }
 const starwars = new Movie('starwars', 'Sci-Fi', 1976);
 
@@ -17,6 +15,6 @@ const theterminator = new Movie('the terminator', 'Sci-Fi', 1984);
 const groundhogday = new Movie('groundhog day', 'Comedy', 1993);
 
 const movies = [starwars, theterminator, groundhogday];
-movies.forEach(Movie => {
-    Movie.print();
+movies.forEach(movie => {
+    movie.print();
 });
