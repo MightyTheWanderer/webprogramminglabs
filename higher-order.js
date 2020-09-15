af501ff129bf3db9b7d125883c52ad72363e450a
MightyTheWanderer <39923450+MightyTheWanderer@users.noreply.github.com>
2020-09-14 23:29:18 -0400

    Create higher-order.js

---
+++ higher-order.js
@@ -0,0 +1,16 @@
+function count(arr, callback) {
+    return arr.reduce((count, z) => callback(z) ? count + 1 : count, 0);
+
+}
+
+const numbers = [1, 2, -3, 4, 5, -6, 7, 8];
+
+const evens = count(numbers, n => n % 2 === 0);
+console.log('evens:', evens);
+
+const negatives = count(numbers, n => n < 0);
+console.log('negatives:', negatives);
+
+const fruit = ['apple', 'orange', 'apple', 'banana', 'kiwi', 'apple'];
+const apples = count(fruit, f => f === 'apple');
+console.log('apples:', apples);
