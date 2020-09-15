88d368fadc974fc37a0fc4212a0330cc83c5157d
MightyTheWanderer <39923450+MightyTheWanderer@users.noreply.github.com>
2020-09-14 23:41:33 -0400

    Create classes.js

---
+++ classes.js
@@ -0,0 +1,31 @@
+class Pet {
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak() {
+        console.log(`${this.name} makes noise`);
+    }
+}
+class Dog extends Pet {
+    constructor(name) {
+        super(name);
+    }
+    speak() {
+        console.log(`${this.name} says woof woof`);
+    }
+}
+class Cat extends Pet {
+    constructor(name) {
+        super(name);
+    }
+    speak() {
+        console.log(`${this.name} says meow`);
+    }
+}
+
+const pets = [
+    new Dog('Mitzie'),
+    new Cat('Felix')
+];
+pets.forEach(pet => pet.speak());
