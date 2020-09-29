2fa50af4b586fc6bc77838396f2fb28ae47af5e5
MightyTheWanderer <39923450+MightyTheWanderer@users.noreply.github.com>
2020-09-29 10:52:25 -0400

    Create calculator.test.js

---
+++ calculator.test.js
@@ -0,0 +1,15 @@
+To check answer open console
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+function subtract(a,b){
+return a-b;
+}
+console.log(("It should resolve with a 'Your data has arrived")subtract(4,2));
+</script>
+
+</body>
+</html>
