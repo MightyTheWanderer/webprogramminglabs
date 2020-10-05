5253a798e3300f012e40913009a223ad15368840
MightyTheWanderer <39923450+MightyTheWanderer@users.noreply.github.com>
2020-10-05 14:30:13 -0400

    Create promises.js

---
+++ promises.js
@@ -0,0 +1,13 @@
+function getData(callback) {
+    return new Promise((resolve, reject) => {
+        const data = [1, 2, 3];
+        setTimeout(() => resolve(data), 1000);
+    });
+}
+
+getData().then(data => {
+    const sum = data.reduce((a, b) => a + b, 0);
+    console.log(sum);
+});
+
+/* compiled in vs before submitting */
